082e5b71e0159333425ee30d55fbdd2048b1633c
Carlos8711 <112145939+Carlos8711@users.noreply.github.com>
2024-05-27 15:28:18 -0300

    update-form

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 
 
 let viewform=document.getElementById("conteiner-form")
+let addSt=document.getElementById("addstyle")
+let reSt=document.getElementById("removestyle")
+let liul=document.getElementById('li')
 
  function btnCancelMensege(){
    viewform.classList.add('closeform')
@@ -10,4 +13,19 @@ let viewform=document.getElementById("conteiner-form")
    viewform.classList.remove('closeform')
 
  }
+ addSt.addEventListener('click',function(){
+  liul.style.left="65%"
+  addSt.style.display="none"
+  reSt.style.display="flex"
+
+ })
+ reSt.addEventListener('click',function(){
+  liul.style.left="100%"
+  addSt.style.display="flex"
+  addSt.style.justifyContent="center"
+  addSt.style.alignItems="center"
+  reSt.style.display="none"
+ })
+
+  
 
